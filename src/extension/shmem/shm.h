c0b5eb87859799d19136f38f7d3cdbfb324ed4a4
Corbin Champion <corbinlc@gmail.com>
2019-04-29 15:02:02 -0700

    remove all use of sockets and pthreads

--- src/extension/shmem/shm.h
+++ src/extension/shmem/shm.h
@@ -11,18 +11,6 @@ __BEGIN_DECLS
 # define shmid_ds shmid64_ds
 #endif
 
-/* Shared memory control operations. */
-extern int shmctl(int shmid, int cmd, struct shmid_ds* buf);
-
-/* Get shared memory area identifier. */
-extern int shmget(key_t key, size_t size, int shmflg);
-
-/* Attach shared memory segment. */
-extern void *shmat(int shmid, void const* shmaddr, int shmflg);
-
-/* Detach shared memory segment. */
-extern int shmdt(void const* shmaddr);
-
 __END_DECLS
 
 #endif
